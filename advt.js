59b81bfd0290c31b353968cc7b3af99833b17d25
abel kl bil <abelkbil@rediffmail.com>
2013-06-20 17:11:54 +0530

    acess golbal var js

--- advt.js
+++ advt.js
@@ -1,1 +1,16 @@
-(function() {console.log("Node count: ");})();
+(function() {
+	fb_ad_width=240;
+	console.log(fb_ad_client);
+	function getWidth()
+  {
+    xWidth = null;
+    if(window.screen != null){
+      xWidth = window.screen.availWidth;
+      fb_ad_width = xWidth;
+      }
+  }
+  getWidth();
+  document.write("CLIENT ID :- "+fb_ad_client+"<br>");
+  document.write("SCREEN WIDTH :- "+fb_ad_width);
+
+	})();
