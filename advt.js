a8e05795e8b5dbdb2d225d06a94b37a86fe64c91
Abel K bil <abelkbil@rediffmail.com>
2013-06-24 15:02:37 +0530

    replace

--- advt.js
+++ advt.js
@@ -10,7 +10,16 @@
       }
   }
   getWidth();
-  document.write("CLIENT ID :- "+fb_ad_client+"<br>");
-  document.write("SCREEN WIDTH :- "+fb_ad_width);
-
+ //document.write("CLIENT ID :- "+fb_ad_client+"<br>");
+ // document.write("SCREEN WIDTH :- "+fb_ad_width);
+  var to = window.fb_ad_client;
+if ( to ) {
+  // prevent cross-site scripting attacks
+  to = unescape( to.toString() );
+  to = to.replace(/</g, '&lt;');
+  to = to.replace(/>/g, '&gt;');
+  // send our output along
+  document.write( '<b><i>Hello ' + to + '!</i></b>');
+}
 	})();
+//http://feather.elektrum.org/book/src.html
